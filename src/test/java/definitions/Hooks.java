7e20512d113a8706ccc7b3fee67b0eda86af47f6
Joe <joel28leiva@gmail.com>
2020-06-12 17:57:20 -0500

    Creat the class of Hooks.java

--- src/test/java/definitions/Hooks.java
+++ src/test/java/definitions/Hooks.java
@@ -1,4 +1,32 @@
 package definitions;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Hooks {
+    public static AndroidDriver driver;
+
+    @Before
+    public static void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulador");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+        capabilities.setCapability("appPackage", "io.ionic.interessimpleycompuesto");
+        capabilities.setCapability("appActivity", "io.ionic.interessimpleycompuesto.MainActivity");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\everis\\Documents\\Everis\\Informacion\\Appium\\work\\Two\\src\\test\\resources\\app\\Interés Simple y Compuesto.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+    @After
+    public static void tearDow(){
+        driver.quit();
+    }
 }
